89e824cf39400c01a7eab7dfbd58e9270f35ce22
Tiny91 <tinyfradet@gmail.com>
2021-08-25 10:33:32 +0200

    initial frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/groupomania.png">
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+   margin-top: 60px;
+}
+</style>
